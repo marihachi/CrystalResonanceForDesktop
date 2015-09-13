8496239cb1d8308723e77eba849a23a9f3d3eccb
marihachi <marihachi0620@gmail.com>
2015-09-13 17:05:27 +0900

    Update Title menu

--- src/State/GameTitle.hpp
+++ src/State/GameTitle.hpp
@@ -13,6 +13,7 @@ private: GameTitle() { }
 private:
 	bool IsInitial = true;
 	int logoHandle;
+	int fontHandle;
 
 public:
 	// 場面名を取得します
@@ -29,6 +30,7 @@ public:
 			IsInitial = false;
 
 			logoHandle = LoadGraph("Image/logo.png", 1);
+			fontHandle = CreateFontToHandle("メイリオ", 25, 5, DX_FONTTYPE_ANTIALIASING_8X8);
 		}
 	}
 
@@ -37,16 +39,43 @@ public:
 	{
 		if (e.IsActive())
 		{
-			Point screenRightBottom = Core::GetInstance().ScreenSize.GetWidthHeightAsPoint();
+			auto screenRightBottom = Core::GetInstance().ScreenSize.GetWidthHeightAsPoint();
+			auto screenCenter = screenRightBottom / 2;
 
 			int imageSize[2];
 			GetGraphSize(logoHandle, &imageSize[0], &imageSize[1]);
-			Point imageRightBottom = Point(imageSize[0], imageSize[1]);
+			auto logoRightBottom = Point(imageSize[0], imageSize[1]);
+			auto logoCenter = logoRightBottom / 2;
 
-			Point location = screenRightBottom / 2 - imageRightBottom / 2;
-			location.AddY(-150);
+			// logo
+			auto logoLocation = screenCenter - logoCenter;
+			logoLocation.AddY(-150);
+			DrawGraph(logoLocation.GetX(), logoLocation.GetY(), logoHandle, 1);
 
-			DrawGraph(location.GetX(), location.GetY(), logoHandle, 1);
+			// menu
+			auto temp = screenCenter;
+			Size boxSize(screenRightBottom.GetX() / 3, 40);
+
+			temp.AddY(100);
+			auto str = "Start";
+			SetDrawBlendMode(DX_BLENDMODE_ALPHA, (int)(255 * 0.7));
+			DrawBox(temp.GetX() - boxSize.GetWidth() / 2, temp.GetY() - boxSize.GetHeight() / 2, temp.GetX() + boxSize.GetWidth() / 2, temp.GetY() + boxSize.GetHeight() / 2, 0xffffff, 0);
+			SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+			DrawStringToHandle(temp.GetX() - GetDrawStringWidthToHandle(str, strlen(str), fontHandle) / 2, temp.GetY() - 25 / 2, str, 0xffffff, fontHandle);
+
+			temp.AddY(60);
+			str = "Setting";
+			SetDrawBlendMode(DX_BLENDMODE_ALPHA, (int)(255 * 0.7));
+			DrawBox(temp.GetX() - boxSize.GetWidth() / 2, temp.GetY() - boxSize.GetHeight() / 2, temp.GetX() + boxSize.GetWidth() / 2, temp.GetY() + boxSize.GetHeight() / 2, 0xffffff, 0);
+			SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+			DrawStringToHandle(temp.GetX() - GetDrawStringWidthToHandle(str, strlen(str), fontHandle) / 2, temp.GetY() - 25 / 2, str, 0xffffff, fontHandle);
+
+			temp.AddY(60);
+			str = "End";
+			SetDrawBlendMode(DX_BLENDMODE_ALPHA, (int)(255 * 0.7));
+			DrawBox(temp.GetX() - boxSize.GetWidth() / 2, temp.GetY() - boxSize.GetHeight() / 2, temp.GetX() + boxSize.GetWidth() / 2, temp.GetY() + boxSize.GetHeight() / 2, 0xffffff, 0);
+			SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+			DrawStringToHandle(temp.GetX() - GetDrawStringWidthToHandle(str, strlen(str), fontHandle) / 2, temp.GetY() - 25 / 2, str, 0xffffff, fontHandle);
 		}
 	}
 };
