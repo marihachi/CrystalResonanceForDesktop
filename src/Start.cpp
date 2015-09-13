7d14de58e2fc72287734c6b0b78ddd548b8671eb
marihachi <marihachi0620@gmail.com>
2015-09-13 10:52:21 +0900

    Add comment

--- src/Start.cpp
+++ src/Start.cpp
@@ -12,22 +12,27 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 {
 	Core &core = Core::GetInstance();
 
+	// 初期化
 	if (!core.Initialize("Crystal Resonance for Desktop", 1280, 720, 82, 195, 202))
 		return -1;
 
+	// 場面の追加
 	core.AddState(&GameTitle::GetInstance());
 	core.AddState(&GameMain::GetInstance());
 	core.AddState(&GameResult::GetInstance());
 	core.AddState(&GameSetting::GetInstance());
 
+	// 現在の場面を設定
 	core.SetNowStateName("Title");
 
+	// ゲームループ
 	while (core.ProcessContext())
 	{
 		core.UpdateTriger();
 		core.DrawTriger();
 	}
 
+	// 解放
 	core.Finalize();
 
 	return 0;
