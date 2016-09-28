8d1e72ce3f5a39f717fdff40ba55e2531e4a3929
Marihachi <marihachi0620@gmail.com>
2016-09-29 00:54:59 +0900

    add files

---
+++ src/Program.cs
@@ -0,0 +1,37 @@
+﻿using DxSharp;
+using DxSharp.Storage;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CrystalResonanceDesktop
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var core = SystemCore.Initialize(new Size(1280, 720), "Crystal Resonance Desktop"))
+			{
+				try
+				{
+					var sceneManager = new SceneStorage();
+					sceneManager.TargetScene = sceneManager.FindByName("TestScene");
+
+					SystemCore.Instance.FontStorage.Add("てすと", "メイリオ", 16);
+
+					while (core.Update())
+					{
+						sceneManager.Update();
+						sceneManager.Draw();
+
+						Utility.FpsHelper.Instance.Wait();
+					}
+				}
+				catch(Exception ex)
+				{
+					MessageBox.Show($"[内容]:\r\n{ex.Message}\r\n\r\n[スタックトレース]:\r\n{ex.StackTrace}", $"実行時エラー({ex.GetType()})", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+			}
+		}
+	}
+}
---
+++ src/Scenes/TestScene.cs
@@ -0,0 +1,21 @@
+﻿using DxSharp;
+using DxSharp.Data;
+using System.Drawing;
+
+namespace CrystalResonanceDesktop.Scenes
+{
+	class TestScene : IScene
+	{
+		private int count = 0;
+
+		public void Update()
+		{
+			count++;
+		}
+
+		public void Draw()
+		{
+			SystemCore.Instance.FontStorage.Item("てすと").Draw(new Point(0, 100), $"カウント値: {count}", Color.Black);
+		}
+	}
+}
