63f6d30c2cad9f6a8c3dac77ad451a9f9db13e6e
Marihachi <marihachi0620@gmail.com>
2016-11-22 00:41:42 +0900

    add ScrollableListControl

---
+++ src/Data/Control/ScrollableListControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CrystalResonanceDesktop.Data.Control
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class ScrollableListControl<T> : ListControl<T> where T: Control
+	{
+		public ScrollableListControl(Point location, int padding, Size frameSize, Orientation orientation = Orientation.Vertical)
+			: base(location, padding, orientation)
+		{
+			FrameSize = frameSize;
+		}
+
+		public Size FrameSize { get; set; }
+
+		public Point ScrollLocation { get; set; }
+
+		/// <summary>
+		/// コントロールがスクロールされたときに発生します
+		/// </summary>
+		public event EventHandler<EventArgs> Scroll;
+
+		/// <summary>
+		/// 項目の Scroll イベントを発生させます
+		/// </summary>
+		/// <param name="e">イベント情報</param>
+		protected virtual void OnScroll(EventArgs e)
+		{
+			Scroll?.Invoke(this, e);
+		}
+
+		/// <summary>
+		/// 状態を更新します
+		/// </summary>
+		public override void Update()
+		{
+			base.Update();
+		}
+
+		/// <summary>
+		/// コントロールを描画します
+		/// </summary>
+		public override void Draw()
+		{
+			base.Draw();
+
+			foreach (var item in Items)
+			{
+				item.Draw();
+			}
+		}
+	}
+}
